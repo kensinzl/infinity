f46a615efa7e9e02d71c25258df08722d280e73d
Unknown <zlchldjyy@gmail.com>
2019-08-29 21:59:38 +1200

    INFINITY-1_configure the Log4j for the springboot hibernate

--- src/main/java/nz/co/indepth/infinity/controller/MovieController.java
+++ src/main/java/nz/co/indepth/infinity/controller/MovieController.java
@@ -4,6 +4,8 @@ import nz.co.indepth.infinity.entity.Movie;
 import nz.co.indepth.infinity.mapper.MovieMapper;
 import nz.co.indepth.infinity.po.MoviePO;
 import nz.co.indepth.infinity.serviceimpl.MovieServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,10 +22,13 @@ public class MovieController {
     @Autowired
     private MovieServiceImpl movieService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
+
     @PostMapping
     public MoviePO createMovie(@RequestBody MoviePO moviePO) {
         // TODO: search springboot has the validator or not
         Movie movie = movieService.createMovie (moviePO);
+        LOGGER.debug ("---------- createMovie");
         return movieMapper.movieToPo (movie);
     }
 
